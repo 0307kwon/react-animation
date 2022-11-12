e3c6a9bbf6937f224b591e89dc7eefbc1ab451e3
0307kwon <0307kwon@gmail.com>
2022-11-12 20:35:05 +0900

    fix: remove less file from package

--- webpack.config.js
+++ webpack.config.js
@@ -2,82 +2,86 @@ const path = require("path");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
-	mode: "production",
-	entry: path.resolve(__dirname, "src", "index.ts"),
-	output: {
-		path: path.resolve(__dirname, "lib"),
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(tsx|ts)$/,
-				use: [
-					{
-						loader: "babel-loader",
-						options: {
-							presets: [
-								"@babel/preset-env",
-								"@babel/preset-react",
-								"@babel/preset-typescript",
-							],
-							plugins: ["@babel/plugin-transform-runtime"],
-						},
-					},
-				],
-			},
-			{
-				test: /\.less$/,
-				exclude: /\.module\.less$/,
-				use: [
-					{
-						loader: "style-loader",
-					},
-					{
-						loader: "css-loader", // translates CSS into CommonJS
-					},
-					{
-						loader: "less-loader", // compiles Less to CSS
-						options: {
-							lessOptions: {
-								// If you are using less-loader@5 please spread the lessOptions to options directly
-								javascriptEnabled: true,
-								relativeUrls: false,
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /\.module\.less$/,
-				use: [
-					"style-loader",
-					{
-						loader: "css-loader",
-						options: {
-							modules: {
-								localIdentName: "[name]__[local]--[hash:base64:5]",
-							},
-						},
-					},
-					{
-						loader: "less-loader",
-						options: {
-							lessOptions: {
-								javascriptEnabled: true,
-							},
-						},
-					},
-				],
-			},
-		],
-	},
-	resolve: {
-		extensions: [".tsx", ".ts", ".js", ".jsx"],
-		alias: {
-			"@": path.resolve(__dirname, "src"),
-		},
-	},
-	plugins: [new ForkTsCheckerWebpackPlugin()],
-	cache: false,
+  mode: "production",
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "lib"),
+    library: "@0307kwon/react-animation",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+              plugins: ["@babel/plugin-transform-runtime"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        exclude: /\.module\.less$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader", // compiles Less to CSS
+            options: {
+              lessOptions: {
+                // If you are using less-loader@5 please spread the lessOptions to options directly
+                javascriptEnabled: true,
+                relativeUrls: false,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.module\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+  cache: false,
 };
